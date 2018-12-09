78ff5448b225bf3592dccaa783e2e0c07d1ef64c
natertater23 <angelbaseball2@gmail.com>
2018-12-09 02:57:47 -0800

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,9 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Frame;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Button;
 
 public class GUI extends JFrame {
 
@@ -29,12 +35,28 @@ public class GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GUI() {
+		setTitle("Battleship\r\n");
+		GridButtonPanel grid =  new GridButtonPanel();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 789, 574);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		
+	
+		
+		JPanel panel = grid.createGridPanel();
+		panel.setBounds(0, 0, 620, 455);
+		contentPane.add(panel);
+		
+		
+	
+		
+		
+		
 
+		
+	}
 }
