2d4728d58c68a4364bb80826a8f7cf3626b139bc
natertater23 <angelbaseball2@gmail.com>
2018-12-10 22:18:43 -0800

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -10,6 +10,8 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -18,18 +20,29 @@ import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 public class GUI extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private JTextField textField;
 	private GridButtonPanel grid =  new GridButtonPanel();
 	private Battleship bs = new Battleship();
 	private Player userPlayer = new Player();
 	private Player computer = new Player();
+	private JLabel lblNewLabel;
+	private JLabel lblComputerPoints;
+	Image hit;
+	Image miss;
+	ImageIcon hitIcon;
+	ImageIcon missIcon;
+	JTextArea textArea = new JTextArea();
 	static GUI frame;
 	/**
 	 * Launch the application.
@@ -54,7 +67,7 @@ public class GUI extends JFrame implements ActionListener {
 		setTitle("Battleship\r\n");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 789, 574);
+		setBounds(100, 100, 635, 564);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -66,36 +79,39 @@ public class GUI extends JFrame implements ActionListener {
         bs.setupComputer(computer);//random ship placement
       
 		
-	
-		ImageIcon hitIcon = new ImageIcon("___");
-		ImageIcon missIcon = new ImageIcon("___");
 
-		Image hit = hitIcon.getImage().getScaledInstance(5,5, java.awt.Image.SCALE_SMOOTH);
-		Image miss = missIcon.getImage().getScaledInstance(5,5, java.awt.Image.SCALE_SMOOTH);
-		
+		 hitIcon = new ImageIcon("D:\\Eclipse\\Battleship\\src\\hit.png");
+		 missIcon = new ImageIcon(" D:\\Eclipse\\Battleship\\src\\miss.png");
+
+		 	hit = hitIcon.getImage().getScaledInstance(5,5, java.awt.Image.SCALE_SMOOTH);
+		 	miss = missIcon.getImage().getScaledInstance(5,5, java.awt.Image.SCALE_SMOOTH);
 		
 		
 		JPanel panel = grid.createGridPanel();
 		panel.setBounds(0, 0, 620, 455);
 		contentPane.add(panel);
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setBounds(10, 432, 309, 92);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Points: ");
-		lblNewLabel.setBounds(344, 432, 120, 23);
+		lblNewLabel = new JLabel("Points: " + computer.oppGrid.getPoints());
+		lblNewLabel.setBounds(343, 467, 120, 23);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblComputerPoints = new JLabel("Computer Points:");
-		lblComputerPoints.setBounds(488, 436, 120, 14);
+		lblComputerPoints = new JLabel("Computer Points: " + userPlayer.oppGrid.getPoints());
+		lblComputerPoints.setBounds(343, 498, 120, 14);
 		contentPane.add(lblComputerPoints);
 		
-		JLabel lblToWin = new JLabel("17 to win");
-		lblToWin.setBounds(418, 418, 46, 14);
+		JLabel lblToWin = new JLabel("17 points to win");
+		lblToWin.setBounds(342, 454, 97, 14);
 		contentPane.add(lblToWin);
+		
+		
+		textArea.setEditable(false);
+		//textArea.setBounds(10, 459, 323, 66);
+		//contentPane.add(textArea);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		scrollPane.setBounds(10,459,323,66);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		contentPane.add(scrollPane);
 		 for (int i = 0; i < 10 * 10; i++) {
 	            int row = i / 10;
 	            int col = i % 10;
@@ -119,38 +135,47 @@ public class GUI extends JFrame implements ActionListener {
           
          
             result = bs.askForGuess(userPlayer, computer,grid.getRow(source),grid.getCol(source));
-            System.out.println(result); // add to textField 
-		
-	    if(result = "HIT") 
-	   	 btn.setImage(hit);
-	    else
-		 btn.setImagae(miss);
+            lblNewLabel.setText("Points: " + userPlayer.oppGrid.getPoints());
+            textArea.append("\n" +result + " at " + grid.getRow(source) + ", "  + grid.getCol(source)); // add to textField 
+        	
+    	    if(result == "HIT") 
+    	   	 btn.setIcon(hitIcon);
+    	    else
+    		 btn.setIcon(missIcon);
          
-		
             if (computer.playerGrid.hasLost())
             {
-                System.out.println("HIT!...COMPUTER LOSES"); //add to textField
+            	   textArea.append( "\n" + "HIT!...COMPUTER LOSES"); //add to textField
+                try {
+					TimeUnit.SECONDS.sleep(4);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
                 frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
                 
             }
             
-            System.out.println("\nCOMPUTER IS MAKING GUESS..."); //add to textField
+            textArea.append("\nCOMPUTER IS MAKING GUESS..."); //add to textField
               
               
             compResult = bs.compMakeGuess(computer, userPlayer);
-            //add time delay
-            System.out.println(compResult); //add to textField
+            lblComputerPoints.setText("Computer Points: " + computer.oppGrid.getPoints());
+            textArea.append( "\n" + compResult); //add to textField
             
-		
             if (userPlayer.playerGrid.hasLost())
             {
-                System.out.println("COMPUTER HITS!...USER LOSES"); //add to textField
-                //add time delay
+            	   textArea.append( "\nCOMPUTER HITS!...USER LOSES"); //add to textField
+                try {
+					TimeUnit.SECONDS.sleep(4);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
                 frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
                 
             }
             
         }
-        
 	}
 
